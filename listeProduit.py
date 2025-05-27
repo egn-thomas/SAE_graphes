33c5ef67aa53626578031e09e7d036fce93e57ed
celien-svg <celien.kerckhove@outlook.fr>
2025-05-27 15:15:22 +0200

    création de la lecture du csv

--- listeProduit.py
+++ listeProduit.py
@@ -1,1 +1,14 @@
-import scv
+import csv
+class csvLoader :
+    def __init__(self, file_path, delimiter=';'):
+        """le delimiter est mis car le fichier csv separe les produit par des ';' et non des ',' """
+        self.file_path = file_path
+        self.delimiter=delimiter
+    def extract_all(self):
+        """on extrait l'ensemble du fichier dans une liste de liste"""
+        with open(self.file_path, newline='', encoding='utf-8') as csvfile:
+            reader=csv.reader(csvfile, delimiter=self.delimiter)
+            return [row for row in reader]
+            
+csv_loader=csvLoader('liste_produits.csv')
+liste_prooduits= csv_loader.extract_all()
