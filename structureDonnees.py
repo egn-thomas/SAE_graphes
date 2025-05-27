5f14aa43dcce76f63facd11ff1b9addedb7ad4cf
Timeodoffagne <tdoffagne@gmail.com>
2025-05-27 16:08:53 +0200

    Update structureDonnees.py

--- structureDonnees.py
+++ structureDonnees.py
@@ -1,27 +1,12 @@
-import csv
 import pandas as pd
 
-with open('liste_produits.csv', mode='r', encoding='utf-8') as produits:     # Lit le csv
-    df = pd.read_csv("liste_produits.csv")
-    df.columns = df.columns.str.strip()
-    csv.DictReader(produits, delimiter=';')
-    pd.read_csv("data.csv", sep=';')
+df = pd.read_csv("liste_produits.csv", sep=';')
+df.columns = df.columns.str.strip()  # Nettoie les noms de colonnes
 
-legumes = df["Légumes"].dropna().tolist()               # Toutes les catégories sont classées ici, l'administrateur peut ajouter des éléments à cette liste
-poissons = df["Poissons"].dropna().tolist()
-viande = df["Viandes"].dropna().tolist()
-epicerie = df["Épicerie"].dropna().tolist()
-epicerieSucree = df["Épicerie sucrée"].dropna().tolist()
-petitDejeuner = df["Petit déjeuner"].dropna().tolist()
-fruits = df["Fruits"].dropna().tolist()
-rayonFrais = df["Rayon frais"].dropna().tolist()
-cremerie = df["Crèmerie"].dropna().tolist()
-conserves = df["Conserves"].dropna().tolist()
-apperitifs = df["Apéritifs"].dropna().tolist()
-boissons = df["Boissons"].dropna().tolist()
-articleMaison = df["Articles Maison"].dropna().tolist()
-hygiene = df["Hygiène"].dropna().tolist()
-bureau = df["Bureau"].dropna().tolist()
-animaux = df["Animaux"].dropna().tolist()
+# Dictionnaire où chaque clé est le nom d'une colonne, et la valeur est la liste des produits
+categories = {
+    colonne: df[colonne].dropna().tolist()
+    for colonne in df.columns
+}
 
-# Ajouter "import structureDonnees" au debut du code Admin
+# liste_catégories : ["Légumes","Poissons","Viandes","Épicerie","Épicerie sucrée","Petit déjeuner","Fruits","Rayon frais","Crèmerie","Conserves","Apéritifs","Boissons","Articles Maison","Hygiène","Bureau","Animaux"]
