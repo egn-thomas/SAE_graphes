36bbe786eab30c2a3bb80db0b2bba81ebecbf58b
egn-thomas <thomas.eugene.62250@gmail.com>
2025-06-10 12:38:40 +0200

    Update modelAdmin.py

--- src/modelAdmin.py
+++ src/modelAdmin.py
@@ -12,13 +12,11 @@ class MagasinModel:
         self.nb_colonnes = 35
         self.nb_lignes = 52
 
+        self.articles_par_cellule = {}
+
         self.produits_par_categorie = {}
         self.categories = []
         self.liste_produits = []
-
-        self.case = (str, bool, [])
-        self.plan = []
-        self.articles_par_cellule = {}
     
     def ajouter_article(self, ligne, colonne, article):
         """Ajoute un article dans une cellule"""
@@ -100,5 +98,30 @@ class MagasinModel:
     def get_produit_a_position(self, ligne, colonne):
         """Retourne le produit à une position donnée"""
         # todo
+
+
+class Cellule:
+    """Les cellules du magazin"""
+    def __init__(self, ligne, colonne, rayon=False):
+        self.ligne = ligne
+        self.colonne = colonne
+        self.rayon = rayon
+        self.articles = []
+        self.voisins = []
+
+    def ajouter_voisin(self, cellule):
+        """Ajoute un voisin"""
+    
+    def est_accessible(self, autre_cellule):
+        """Vérifie si deux cellules sont accessibles entre elles"""
+        return (autre_cellule in self.voisins and 
+                (not self.est_rayon or not autre_cellule.est_rayon))
+
+class Graphe:
+    """Le graphe représentatif du magazin"""
+    def __init__(self, nb_lignes, nb_colonnes):
+        self.nb_lignes = nb_lignes
+        self
+
 
 
