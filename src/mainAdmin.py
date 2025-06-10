b555701fabfedd6a33cd9d03958219c1e81d506e
Laranimrode <larafy1974@gmail.com>
2025-06-10 12:58:33 +0200

    implémentation_mot_de_passe
    Voici le mot de passe.

--- src/mainAdmin.py
+++ src/mainAdmin.py
@@ -1,14 +1,31 @@
 import sys
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QInputDialog,QMessageBox,QLineEdit
 from controllerAdmin import MagasinController
 
 
-def main():
-    app = QApplication(sys.argv)
-    controller = MagasinController()
-    controller.vue.show()
-    sys.exit(app.exec())
-
+def mot_de_passe(max_attempts=3):
+    for attempt in range(max_attempts):
+        password, ok = QInputDialog.getText(None, "Authentification", f"Tentative {attempt + 1}/{max_attempts}\nEntrez le mot de passe:", echo=QLineEdit.EchoMode.Password)
+        if ok and password == "mot":
+            return True
+        elif ok: 
+            QMessageBox.warning(None, "Erreur", "Mot de passe incorrect")
+        else:
+            return False
+    QMessageBox.critical(None, "Bloqué", "Trop de tentatives échouées !")
+    return False
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    
+    if mot_de_passe():
+        controller = MagasinController()
+        controller.vue.show()
+        app.exec()
+    else:
+        print("Authentification échouée ou annulée")
+        sys.exit()
+
+
+
+    
